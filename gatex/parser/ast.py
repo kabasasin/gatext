3585732c09d0a2beb19d357953811afee881df13
Gaba <alkenepan@gmail.com>
2020-04-01 17:17:34 +0800

    using pandoc data type build ast

--- gatex/parser/ast.py
+++ gatex/parser/ast.py
@@ -1,38 +1,58 @@
-class Node(object):
-    name: str = ""
-    content: str = ""
-    children = []
+from pandocfilters import *
 
-    def __repr__(self):
-        res = []
-        for n in self.children:
-            res.append(str(n))
-        return {"{}: {}".format(self.name, self.content): res}
+version = {"pandoc-api-version": [1, 20]}
+meta = {"meta": {}}
 
 
-class TagStart(Node):
-    name: str = "TAG-START"
+def inline_funcs_builder(func_name: str, content: str) -> dict:
+    if func_name in ["Str", "Emph", "Strong"]:
+        return eval(func_name)(content)
+    elif func_name in ["Code"]:
+        return eval(func_name)(["", [], []], content)
+    else:
+        raise Exception("{} is not available func name")
 
 
-class TagStop(Node):
-    name: str = "TAG-STOP"
+def block_funcs_builder(func_name: str, content: str) -> dict:
+    if func_name in ["Plain", "Para", "BulletList"]:
+        return eval(func_name)(content)
+    elif func_name in ["CodeBlock"]:
+        return eval(func_name)(["", [], []], content)
+    else:
+        raise Exception("{} is not available func name")
 
 
-class TagName(Node):
-    name: str = "TAG-NAME"
+class Tag(object):
+    # inline element
+    def __init__(self, name: str, content: str):
+        self.name = name
+        self.content = content
+
+    def build(self, render_mapping: dict) -> dict:
+        func_name = render_mapping.get(self.name, "Plain")
+        return inline_funcs_builder(func_name, self.content)
 
 
-class Tag(Node):
-    name: str = "TAG"
+class Mark(Tag):
+    pass
 
 
-class NestedStart(Node):
-    name: str = "NestedStart"
+class TagBlock(object):
+    # Block element
+    def __init__(self, name: str, content: str):
+        self.name = name
+        self.content = content
+
+    def build(self, render_mapping: dict) -> dict:
+        func_name = render_mapping.get(self.name, "Plain")
+        return block_funcs_builder(func_name, self.content)
 
 
-class NestedStop(Node):
-    name: str = "NestedStop"
+class Blocks(list):
+    def dumps(self):
+        return json.dumps(self)
 
 
-class Content(Node):
-    name: str = "Content"
+if __name__ == "__main__":
+    # pandoc -s test.md -t json | jq | pandoc -s -f json
+    print(Plain([Str("ads")]))
---
+++ test/markdown/test.md
@@ -0,0 +1,25 @@
+test
+
+# test
+
+* test
+
+```python
+import asd
+asd 
+
+sdaufbasifvbobo;
+
+afboau
+```
+
+test1 test2 *test3*
+
+> asd
+> asd
+>
+
+> asd
+
+
+* asd`1231`asd
